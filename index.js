a02f7c5b527a2808fb4b84dbe84b974012597abc
Faith Fechser <faithfechser@gmail.com>
2023-07-29 03:50:30 -0600

    add view functions

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
-// constant variables
+// Constant variables
 const inquier = require('inquirer');
 const mysql = require('mysql2');
+
+// MySQL connection
+const connection = mysql.createConnection({
+host: 'localhost',
+user: 'root',
+password: '',
+database: 'ETC10-db',
+});
+
+// Department View Function
+function departmentView() {
+    connection.query('SELECT * FROM department', (err, results) => {
+      if (err) throw err;
+      console.table(results);
+      startApp();
+    });
+  }
+  
+  // Profession View Function
+  function professionView() {
+    connection.query('SELECT * FROM profession', (err, results) => {
+      if (err) throw err;
+      console.table(results);
+      startApp();
+    });
+  }
+  
+  // Employee View Function
+  function employeeView() {
+    connection.query('SELECT * FROM employee', (err, results) => {
+      if (err) throw err;
+      console.table(results);
+      startApp();
+    });
+  }
