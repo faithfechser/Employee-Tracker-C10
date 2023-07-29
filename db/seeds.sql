a028da24b63ac601cce5e9f87f21cb0046b333b0
Faith Fechser <faithfechser@gmail.com>
2023-07-29 03:29:11 -0600

    add comments and change role to profession

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,4 @@
+-- department
 INSERT INTO department (department_name) 
 VALUES 
     ('Management'),
@@ -5,8 +6,8 @@ VALUES
     ('Sales'),
     ('Front End'),
     ('Back End');
-
-INSERT INTO role (title, salary, department_name) 
+-- profession/role
+INSERT INTO profession (title, salary, department_name) 
 VALUES
     ('General Manager', 85000, 'Management'),
     ('Assistant Manager', 70000, 'Management'),
@@ -14,10 +15,12 @@ VALUES
     ('Salesman', 55000, 'Sales'),
     ('Front End Associate', 45000, 'Front End'),
     ('Back End Associate', 45000, 'Back End');
-
+-- employee
 INSERT INTO employee (last_name, first_name, department_name, manager) 
 VALUES
     ('Gatsby', 'Jay', 'Management', NULL),
     ('Pines', 'Dipper', 'Back End', 'Jay'),
     ('Undertale', 'Sans', 'Marketer', 'Jay'),
+    ('Iplier', 'Mark', 'Marketer', 'Jay'),
+    ('Schlatt', 'Jonathan', 'Sales', 'Jay'),
     ('Cartman', 'Eric', 'Sales', 'Jay');
