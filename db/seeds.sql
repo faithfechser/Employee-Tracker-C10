5a31f2cd5f58b9c878d885290a2bb64d3ee798a0
Faith Fechser <faithfechser@gmail.com>
2023-08-08 12:19:10 -0600

    debug seeds file

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,26 +1,26 @@
 -- department
-INSERT INTO department (department_name) 
+INSERT INTO department (name) 
 VALUES 
     ('Management'),
     ('Marketing'),
     ('Sales'),
     ('Front End'),
     ('Back End');
--- profession/role
-INSERT INTO profession (title, salary, department_name) 
+-- role
+INSERT INTO role (title, salary, department_id) 
 VALUES
-    ('General Manager', 85000, 'Management'),
-    ('Assistant Manager', 70000, 'Management'),
-    ('Marketer', 55000, 'Marketing'),
-    ('Salesman', 55000, 'Sales'),
-    ('Front End Associate', 45000, 'Front End'),
-    ('Back End Associate', 45000, 'Back End');
+    ('General Manager', 85000, 1),
+    ('Assistant Manager', 70000, 1),
+    ('Marketer', 55000, 2),
+    ('Salesman', 55000, 3),
+    ('Front End Associate', 45000, 4),
+    ('Back End Associate', 45000, 5);
 -- employee
-INSERT INTO employee (last_name, first_name, department_name, manager) 
+INSERT INTO employee (last_name, first_name,role_id, manager_id) 
 VALUES
-    ('Gatsby', 'Jay', 'Management', NULL),
-    ('Pines', 'Dipper', 'Back End', 'Jay'),
-    ('Undertale', 'Sans', 'Marketer', 'Jay'),
-    ('Iplier', 'Mark', 'Marketer', 'Jay'),
-    ('Schlatt', 'Jonathan', 'Sales', 'Jay'),
-    ('Cartman', 'Eric', 'Sales', 'Jay');
+    ('Gatsby', 'Jay', 1, NULL),
+    ('Pines', 'Dipper', 2, 1),
+    ('Iplier', 'Mark', 3, 1),
+    ('Schlatt', 'Jonathan', 3, 1),
+    ('Undertale', 'Sans', 4, 1),
+    ('Cartman', 'Eric', 5, 1);
